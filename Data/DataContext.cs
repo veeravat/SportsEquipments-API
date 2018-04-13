37da21bc2853dc05e74c6f99d6645b68d887fd0b
veeravat <veeravat@me.com>
2018-04-14 02:23:14 +0700

    Initial Commit

---
+++ Data/DataContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using OOAD.Models;
+
+namespace OOAD.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
